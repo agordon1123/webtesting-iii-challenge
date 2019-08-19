f5f55a4d9d169312d8547331914edc7f478c4ba9
Alex <agordon1123@gmail.com>
2019-08-19 16:05:52 -0400

    Snapshots captured

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
-// Test away!
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import Display from './Display';
+
+describe('<Display />', () => {
+    it('Matches snapshot', () => {
+        const tree = renderer.create(<Display />);
+        expect(tree.toJSON()).toMatchSnapshot();
+    });
+});
